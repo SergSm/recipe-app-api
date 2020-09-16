b1a70e0fc8ef059220fc02774b24bdeaf2271fa5
smirnovserg.s <smirnovserg.s@gmail.com>
2020-09-16 22:36:33 +0600

    Is email filled? check and test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,9 +18,15 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password), True)
 
 
-def test_new_user_email_normalized(self):
-	"""Test the email for a new user is normalized"""
-	email = 'test@LONDONAPPDEV.com'
-	user = get_user_model().objects.create_user(email, 'test123')
+    def test_new_user_email_normalized(self):
+    	"""Test the email for a new user is normalized"""
+    	email = 'test@LONDONAPPDEV.com'
+    	user = get_user_model().objects.create_user(email, 'test123')
 
-	self.assertEqual(user.email, email.lower())
+    	self.assertEqual(user.email, email.lower())
+
+
+    def  test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,'test123')
