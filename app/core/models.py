c9896390b6ac88c41768b657acdc0e51bd68b8e9
smirnovserg.s <smirnovserg.s@gmail.com>
2020-09-16 22:15:58 +0600

    Custom User model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new User"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)  # self._db - for different DBs ?
+
+        return user
+
+
+#  PermissionsMixin class is used to work with permissions using our User
+class User(AbstractBaseUser, PermissionsMixin):
+    # don't forget to make email unique as far
+    # we gonna use it as username
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    # This field is a predefined field of AbstractBaseUser
+    USERNAME_FIELD = 'email'
