c89e4ec6511dc9d91380b755ecb3968cb41a135a
smirnovserg.s@gmail.com <smirnovserg.s@gmail.com>
2020-09-18 18:10:32 +0600

    flake8

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,6 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        username = ''
         email = 'me@smirnovsergs.com'
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
@@ -17,19 +16,17 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password), True)
 
-
     def test_new_user_email_normalized(self):
-    	"""Test the email for a new user is normalized"""
-    	email = 'test@LONDONAPPDEV.com'
-    	user = get_user_model().objects.create_user(email, 'test123')
+        """Test the email for a new user is normalized"""
+        email = 'test@LONDONAPPDEV.com'
+        user = get_user_model().objects.create_user(email, 'test123')
 
-    	self.assertEqual(user.email, email.lower())
+        self.assertEqual(user.email, email.lower())
 
-
-    def  test_new_user_invalid_email(self):
+    def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None,'test123')
+            get_user_model().objects.create_user(None, 'test123')
 
     def test_new_superuser(self):
         """Test creating a new superuser"""
