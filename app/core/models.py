cff6e636b06322c5f18a64f4995707fe97347150
SergSm <smirnovserg.s@gmail.com>
2020-10-01 19:42:21 +0600

    tag model typo

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
@@ -39,3 +40,15 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # This field is a predefined field of AbstractBaseUser
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
