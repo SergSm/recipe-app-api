c22a95c3eb962b957b142c3d1da94af924712053
SergSm <smirnovserg.s@gmail.com>
2020-10-02 11:51:03 +0600

    added test for listing and creating tags

---
+++ app/recipe/tests/test_tags.py
@@ -0,0 +1,94 @@
+from django.contrib import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import tag
+
+from recipe.serializers import TagSerializer
+
+
+TAGS_URL = reverse('recipe:tag-list')
+
+
+class PublicTagsApiTest(TestCase):
+    """Test the publicly available tags API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that login required for reetrieving tags"""
+
+        res = self.client.get(TAGS_URL)
+        res.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagsApiTest(TestCase):
+    """Test the authorized user tags API"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'test@Test.com',
+            'password',
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving tags"""
+        Tag.objects.create(user=self.user, name='Vegan')
+        Tag.objects.create(user=self.user, name='Dessert')
+
+        res = self.client.get(TAGS_URL)
+
+        tags = Tag.objects.all().order_by('-name')
+
+        # if a nested representation should be a list of items,
+        # you should pass the many=True flag to the nested serialized.
+        serializer = TagSerializer(tags, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """Test that the returned tags are only for the authenticated user"""
+        user2 = get_user_model().objects.create_user(
+            'other@londonappdev.com',
+            'testpass'
+        )
+
+        Tag.objects.create(user=user2, name='Fruity')  # create 1'st tag
+        tag = Tag.objects.create(user=self.user, name='Comfort food')  # 2'nd
+
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)  # 1 element in the list? we created
+        # more than 1 tag for different users
+
+        self.assertEqual(res.data[0]['name'], tag.name) # and the name of the
+        # created tag must be as we specified for the user2
+
+        # Create 2 functions for testing creating tag
+
+        def create_tag_successful(self):
+            """Test creating a new tag"""
+            payload = {'name': 'Simple'}
+            self.client.post(TAGS_URL, payload)
+
+            exists = Tag.objects.filter(
+                user=self.user,
+                name=payload['name']
+            ).exists()
+
+            self.assertTrue(exists)
+
+        def test_create_tag_invalid(self):
+            """Test creating an invalid tag(without name)"""
+            payload = {'name': ''}
+            self.client.post(TAGS_URL, payload)
+
+            self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
